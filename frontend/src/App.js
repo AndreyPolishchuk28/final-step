08e5bac166690c4369ad157111f985ff4f5dfd41
Maxim Popov <max_popov@hotmail.com>
2019-09-02 17:23:42 +0300

    Create base architecture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Provider} from 'react-redux'
+import store from './redux'
+import {CardPage, CheckoutPage, Footer, Header, HomePage, LoginPage, MyAccountPage, ProductDetailsPage, ProductListPage} from "./components";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Welcome to final project
-    </div>
+      <Provider store={store}>
+          <Router>
+                <div className="App">
+                    <Header/>
+                    <Switch>
+                        <Route path='/' exact component={HomePage}/>
+                        <Route path='/card' component={CardPage}/>
+                        <Route path='/checkout' component={CheckoutPage}/>
+                        <Route path='/login' component={LoginPage}/>
+                        <Route path='/account' component={MyAccountPage}/>
+                        <Route path='/product-list/:category' component={ProductListPage}/>
+                        <Route path='/product/:id' exact component={ProductDetailsPage}/>
+                    </Switch>
+                    <Footer/>
+                </div>
+          </Router>
+      </Provider>
   );
 }
 
---
+++ frontend/src/components/CardPage/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CardPage = () => {
+    return (
+        <div>Card</div>
+    )
+};
