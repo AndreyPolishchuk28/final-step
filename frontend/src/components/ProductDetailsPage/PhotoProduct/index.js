dcafc601ae4c894e306db4ffeca2c8d3ddfa8b86
Senychak Nazar <nazarsenychak1@gmail.com>
2019-09-05 16:40:34 +0200

    maked functional for product details component, to connect with data base

--- frontend/src/components/ProductDetailsPage/PhotoProduct/index.js
+++ frontend/src/components/ProductDetailsPage/PhotoProduct/index.js
@@ -2,42 +2,19 @@ import React from 'react';
 import { Carousel } from 'antd';
 import './styles.scss';
 
-const gitar = [
-	{
-		url: 'https://images.ua.prom.st/1154983329_klasichna-gitara-yamaha.jpg'
-	},
-	{
-		url: 'https://images.ua.prom.st/1154983329_klasichna-gitara-yamaha.jpg'
-	},
-	{
-		url: 'https://images.ua.prom.st/1154983329_klasichna-gitara-yamaha.jpg'
-	},
-	{
-		url: 'https://images.ua.prom.st/1154983329_klasichna-gitara-yamaha.jpg'
-	}
-];
+const PhotoProduct = ({ product }) => {
+	console.log(product);
+	let photo = product.photo;
+	console.log(photo);
+	let caruselItems = photo.map((item) => {
+		return (
+			<div className="photo-product__container">
+				<img src={`/static/img/${item}`} />
+			</div>
+		);
+	});
 
-let url = gitar[0].url;
-
-console.log(gitar[0]);
-
-const CarouselProduct = () => {
-	return (
-		<Carousel autoplay>
-			<div>
-				<img src={url} />
-			</div>
-			<div>
-				<img src={url} />
-			</div>
-			<div>
-				<img src={url} />
-			</div>
-			<div>
-				<img src={url} />
-			</div>
-		</Carousel>
-	);
+	return <Carousel autoplay>{caruselItems}</Carousel>;
 };
 
-export default CarouselProduct;
+export default PhotoProduct;
