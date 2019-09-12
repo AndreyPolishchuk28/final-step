2a9ec3b28c22d38cc5ae0d4f1d3cad76c5057078
Maksim Popov <max.popov.mail@gmail.com>
2019-09-12 15:53:47 +0300

    Add saga

--- frontend/src/components/ProductDetailsPage/InfoProduct/index.js
+++ frontend/src/components/ProductDetailsPage/InfoProduct/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import {connect} from 'react-redux';
 import './styles.scss';
 import { Tabs, Icon } from 'antd';
-import {addToBasket} from "../../../redux/actions";
+import {addToBasket} from "../../../redux";
 
 const { TabPane } = Tabs;
 
@@ -12,7 +12,7 @@ const mapStateToProps = (state) => {
 	return {...state}
 };
 
-const InfoProduct = connect(mapStateToProps)(props => {
+const InfoProduct = connect(mapStateToProps, {addToBasket})(props => {
 	const product = props.product;
 	const productId = product._id;
 
@@ -20,7 +20,7 @@ const InfoProduct = connect(mapStateToProps)(props => {
 	console.log(count);
 
 	const addToBasketRes = async () => {
-		addToBasket(props.dispatch,{
+		props.addToBasket({
 			id: productId,
 			quantity: count
 		})
