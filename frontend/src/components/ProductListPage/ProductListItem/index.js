1d3489ea1d22b3913dc18cc50153dde9c22a887d
Maksim Popov <max.popov.mail@gmail.com>
2019-09-13 21:44:22 +0300

    Remove old sagas, add start basket data, refacroring

--- frontend/src/components/ProductListPage/ProductListItem/index.js
+++ frontend/src/components/ProductListPage/ProductListItem/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {addToBasket} from "../../../redux/actions";
 import {connect} from 'react-redux';
 
 const mapStateToProps = (state) => {
