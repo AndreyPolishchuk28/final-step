2d57319ba4a06435b31bcbe2d552835bdee48e02
Андрей Полищук <ipolishchuk94@gmail.com>
2019-09-16 12:30:56 +0300

    validation

--- frontend/src/components/RegistrationPage/RegistrationPage.js
+++ frontend/src/components/RegistrationPage/RegistrationPage.js
@@ -12,27 +12,37 @@ const mapStateToProps = state =>{
 export const Registration = connect(mapStateToProps,{createNewUser})(props =>{
     const validation = () =>{};
 
-    const [name, setName] = useState();
-    const [lastName, setLastName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [confirmPassword, setConfirmPassword] = useState();
+    const [values, setValues] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+    const [errors, setErrors] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const handleName = event => {
-        setName(event.target.value);
+    const handleChange = event =>{
+        const {name, value} = event.target;
+        setValues({
+            ...values,
+            [name]: value
+        })
     };
-    const handleLastName = event => {
-        setLastName(event.target.value);
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        setErrors(validate(values));
+        setIsSubmitting(true)
     };
-    const handleEmail = event => {
-        setEmail(event.target.value);
-    };
-    const handlePassword = event => {
-        setPassword(event.target.value);
-    };
-    const handleConfirmPassword = event => {
-        setConfirmPassword(event.target.value);
-    };
+
+    useEffect(() =>{
+        if (Object.keys(errors).length === 0 && isSubmitting) {
+            callback();
+        }
+    },[errors]);
+
+
 
     const registrationUser = async () =>{
         props.createNewUser({
@@ -52,23 +62,23 @@ export const Registration = connect(mapStateToProps,{createNewUser})(props =>{
                     <div className='field-wrapper'>
                         <label className='login-input-title'>
                         First name
-                        <input onChange={handleName} name='name' type='text' placeholder='Your name...' className='login-email-input'/>
+                        <input onChange={handleChange} name='name' type='text' placeholder='Your name...' className='login-email-input'/>
                         </label>
                         <label className='login-input-title'>
                             Last name
-                            <input onChange={handleLastName} name='surname' type='text' placeholder='Last name...' className='login-email-input'/>
+                            <input onChange={handleChange} name='surname' type='text' placeholder='Last name...' className='login-email-input'/>
                         </label>
                         <label className='login-input-title'>
                             E-mail
-                            <input onChange={handleEmail} name='email' type='text' placeholder='Your email...' className='login-email-input'/>
+                            <input onChange={handleChange} name='email' type='text' placeholder='Your email...' className='login-email-input'/>
                         </label>
                         <label className='login-input-title'>
                             Password
-                            <input onChange={handlePassword} name='password' type='password' placeholder='Your password...' className='login-email-input'/>
+                            <input onChange={handleChange} name='password' type='password' placeholder='Your password...' className='login-email-input'/>
                         </label>
                         <label className='login-input-title'>
                             Confirm Password
-                            <input onChange={handleConfirmPassword} name='confirm-password' type='password' placeholder='Confirm password...' className='login-email-input'/>
+                            <input onChange={handleChange} name='confirm-password' type='password' placeholder='Confirm password...' className='login-email-input'/>
                         </label>
                     </div>
                 </div>
