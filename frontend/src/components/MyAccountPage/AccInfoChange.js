aea560c2b1c1d9b2f93a58cd7bcbd94880849724
Vanezio <vanezioforever@gmail.com>
2019-09-25 21:08:40 +0300

    checkout and acc warnings for inputs and prod infos are better

--- frontend/src/components/MyAccountPage/AccInfoChange.js
+++ frontend/src/components/MyAccountPage/AccInfoChange.js
@@ -21,28 +21,77 @@ export const AccInfoChange = connect(mapStateToProps, {changeUserInfo}) ((props)
     const [ addChange, setAddChange] = useState({
         ...props.auth.userInfo.def_address
     })
+
+    const [err, setErr] = useState({});
     
     const changeHandler = (event) => {
         setUserChange({
             ...userChange,
-            [event.target.name]: event.target.value
+            [event.target.id]: event.target.value
         })
     }
 
     const changeAdditionalHandler = (event) => {
         setAddChange({
             ...addChange,
-            [event.target.name]: event.target.value 
+            [event.target.id]: event.target.value 
         })
         console.log(addChange);
     }
 
+    function validate(userChange) {
+        let errors = {};
+
+        if(userChange.first_name === ""){
+            errors.firstNameReq = 'First name is required'
+        }
+        if(userChange.last_name === ""){
+            errors.lastNameReq = 'Last name is required'
+        }
+
+        console.log(errors);
+
+        return errors
+    }
+
+
+    const inputAnimF = (event) => {
+        event.target.previousElementSibling.classList.add("change-label-active")
+    }
+
+    const inputAnimB = (event) => {
+        if(!event.target.value){
+        event.target.previousElementSibling.classList.remove("change-label-active")
+        }
+    }
+
+    const changeInput = (strFor, label, def, changeFunc, error) => {
+        if(def !== "" && def){
+            return (
+                <div className="change-input-wrapper">
+                    <label className="change-label change-label-active" for={strFor}>{label}</label>
+                    <input className="input-change" type="text" onChange={changeFunc} id={strFor} defaultValue={def} onFocus={inputAnimF} onBlur={inputAnimB}/>
+                    {error ? <p className='error'>{error}</p> : null}
+                </div>
+            )
+        } else {
+            return (
+                <div className="change-input-wrapper">
+                    <label className="change-label" for={strFor}>{label}</label>
+                    <input className="input-change" type="text" onChange={changeFunc} id={strFor} defaultValue={def} onFocus={inputAnimF} onBlur={inputAnimB}/>
+                    {error ? <p className='error'>{error}</p> : null}
+                </div>
+            )
+        }
+    }
+    
+
     const submitHandler = async () => {
-        if(userChange.first_name < 1){
-            alert('First name must contain atleast 1 charecter')
-        } else if(userChange.last_name < 1){
-            alert('Last name must contain atleast 1 charecter')
-        } else{
+        let errors = validate(userChange)
+        
+        if (Object.keys(errors).length){
+            setErr(validate(userChange))
+        } else {
             const data = await {
                 firstName: userChange.firstName,
                 lastName: userChange.lastName,
@@ -65,16 +114,16 @@ export const AccInfoChange = connect(mapStateToProps, {changeUserInfo}) ((props)
                 <div className="container-change">
                     <i className="fas fa-times close-btn" onClick={() => {props.setPageState({ page: "info"})}}></i>
                     <h1 className="info-header">Main information</h1>
-                    <input className="input-change" type="text" onChange={changeHandler} defaultValue={userChange.firstName} placeholder="First name" name="firstName"/>
-                    <input className="input-change" type="text" onChange={changeHandler} defaultValue={userChange.lastName } placeholder="Last name" name="lastName"/>
+                    {changeInput("first_name", "First name", userChange.firstName, changeHandler, err.firstNameReq)}
+                    {changeInput("last_name", "Last name", userChange.lastName, changeHandler, err.lastNameReq)}
 
                     <Button style={{width: "150px", display: "block"}} type="Deafault" onClick={() => {props.setPageState({ page: "changePassword"})}}>change password</Button>
 
                     <h1 className="info-header">Additional information</h1>
-                    <input className="input-change" type="text" onChange={changeAdditionalHandler} defaultValue={addChange.country } placeholder="country" name="country"/>
-                    <input className="input-change" type="text" onChange={changeAdditionalHandler} defaultValue={addChange.city } placeholder="city" name="city"/>
-                    <input className="input-change" type="text" onChange={changeAdditionalHandler} defaultValue={addChange.address } placeholder="address" name="address"/>
-                    <input className="input-change" type="text" onChange={changeAdditionalHandler} defaultValue={addChange.postal } placeholder="Postal code" name="postal"/>
+                    {changeInput("country", "Country", addChange.country, changeAdditionalHandler)}
+                    {changeInput("city", "City", addChange.city, changeAdditionalHandler)}
+                    {changeInput("address", "Address", addChange.address, changeAdditionalHandler)}
+                    {changeInput("postal", "Postal code", addChange.postal, changeAdditionalHandler)}
                     <button className="submit-btn" onClick={submitHandler}>Submit</button>
                 </div>
                 : null 
--- frontend/src/components/MyAccountPage/FullOrder.js
+++ frontend/src/components/MyAccountPage/FullOrder.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 
 import {connect} from 'react-redux'
 import {getUserInfo} from '../../redux/auth'
@@ -17,14 +17,13 @@ export const FullOrder = connect(mapStateToProps, {getUserInfo})((props) => {
 
     const data = dataArr[0];
 
-    const allOrders = data.product_items.map((item, index) => {
+    const allProducts = data.product_items.map((item, index) => {
             return (
                 <div className="full-order-info__products__items"  key={index}>
-                    <h1 className="full-order-info__products__text">{item.product.name}</h1>
-                    <h1 className="full-order-info__products__text">{item.product.producer}</h1>
-                    <h1 className="full-order-info__products__text">{item.product.category}</h1>
-                    <h1 className="full-order-info__products__text">{item.product.price}</h1>
-                    <h1 className="full-order-info__products__text">{item.product._id}</h1>
+                    <h1 className="full-order-info__products__text">Model: {item.product.name}</h1>
+                    <h1 className="full-order-info__products__text">Category: {item.product.category}</h1>
+                    <h1 className="full-order-info__products__text">Price for one: {item.product.price}$</h1>
+                    <h1 className="full-order-info__products__text">Quantity: {item.quantity}x</h1>
                 </div>
                 )
         })
@@ -35,19 +34,18 @@ export const FullOrder = connect(mapStateToProps, {getUserInfo})((props) => {
             props.auth.userInfo ?
             <div className="full-order-info">
                 <div className="full-order-info__items">
-                    <p>{data.order_no}</p>
-                    <p>{data.creation_date}</p>
-                    <p>{data.order_total} {data.currency}</p> 
-                    <h1>Billing address</h1>
-                    <p>Country: {data.billing_address.country}</p>       
-                    <p>City: {data.billing_address.city}</p>       
-                    <p>Address: {data.billing_address.address}</p>       
-                    <p>Postal: {data.billing_address.postal}</p>  
-                    <p>Orders delivery type: {data.billing_address.delivery}</p>    
-                    <p>{data.customer_info.email}</p>  
+                    <h1 className="full-order-info__items__total">Total price: {data.order_total} {data.currency}</h1> 
+                    <h1 className="full-order-info__items__address-main">Billing address :</h1>
+                    <p className="full-order-info__items__address">Country: {data.billing_address.country}</p>       
+                    <p className="full-order-info__items__address">City: {data.billing_address.city}</p>       
+                    <p className="full-order-info__items__address">Address: {data.billing_address.address}</p>       
+                    <p className="full-order-info__items__address">Postal code: {data.billing_address.postal}</p>                     
+                    <p className="full-order-info__items__delivery">Delivery type: {data.billing_address.delivery}</p>    
+                    <p className="full-order-info__items__email">Used email address: {data.customer_info.email}</p>  
+                    <p className="full-order-info__items__date">Creation date: {data.creation_date}</p>
                 </div>
                 <div className="full-order-info__products">
-                    {allOrders}
+                    {allProducts}
                 </div>
             </div>
             : null
