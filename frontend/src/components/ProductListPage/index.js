1bafcd86472ae026b7e0d7233ba393fb8dc75f28
dipolik4 <dipolik4@gmail.com>
2019-09-25 16:15:51 +0300

    fixed images sizes, and ProductList component

--- frontend/src/components/ProductListPage/index.js
+++ frontend/src/components/ProductListPage/index.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {ProductListItem} from './ProductListItem'
 import './style.scss'
-import {Row, Col, Checkbox, Button, InputNumber } from 'antd';
-
+import {Row, Col, Checkbox} from 'antd';
 import {getProducts} from '../../redux/catalog'
 import {connect} from 'react-redux'
 
@@ -14,6 +13,8 @@ let products;
 let skip = 0;
 let limit= 9;
 let clearOld = true;
+let checkedArr = [];
+let producers=[];
 
 const mapStateToProps = (state) =>  {
     return {
@@ -21,15 +22,14 @@ const mapStateToProps = (state) =>  {
     }
 }
 
-
 export const ProductListPage =  connect (mapStateToProps, {getProducts}) ( (props) => {
-    
+    const [sortFromCheap, setSortFromCheap] = useState([])
+    const [sortStatus, setSortStatus] = useState(true)
     products = props.catalog.products;
-
     const getProductsHandler = () => {
         let payload = {
             category: props.match.params.category,
-            producer:[], 
+            producer: checkedArr,
             skip: skip,
             limit: limit,
             clearOld: clearOld
@@ -37,16 +37,63 @@ export const ProductListPage =  connect (mapStateToProps, {getProducts}) ( (prop
         props.getProducts(payload)
     };
 
+    props.catalog.categories.forEach(element => {
+        if(element.name === props.match.params.category){
+            producers = element.producers              
+            }
+        })
+
+    const checkboxHandler = (e) => {
+        let checked = e.target.name;
+        let index = checkedArr.indexOf(e.target.name)
+        if (e.target.checked === true){
+            checkedArr.push(checked)
+        } else {
+            checkedArr.splice(index, 1)
+        }
+        getProductsHandler()
+    }
+
     const showMoreHandler = () => {
         skip += limit;
         clearOld = false;
         getProductsHandler()
         clearOld = true;
     }
-    useEffect(() => {
-        getProductsHandler();
-    }, []);
 
+    const sortCheap = () => {
+        products.sort((a, b) => (a.price > b.price) ? 1 : -1 );
+        sortingCheapHandler = true;
+        sortingExpensiveHandler = false;
+        setSortStatus(!sortStatus)
+        setSortFromCheap(products)
+    }
+
+    const sortExpensive = () => {
+        products.sort((a, b) => (a.price < b.price) ? 1 : -1 )
+        sortingExpensiveHandler = true;
+        sortingCheapHandler = false;
+        setSortStatus(!sortStatus)
+        setSortFromCheap(products)
+    }
+
+    useEffect(() => {
+        skip = 0
+        sortingCheapHandler = false;
+        sortingExpensiveHandler = false;
+        getProductsHandler();
+        
+    }, [props.match.params.category]);
+
+    useEffect(()=>{
+        if (sortingCheapHandler === true) {
+            sortCheap()
+        } else if (sortingExpensiveHandler === true) {
+            sortExpensive()
+        }
+    }, [products])
+
+  
     return (
         <div className="pagelist-wrapper">
             <div className="page-top-info">
@@ -56,20 +103,21 @@ export const ProductListPage =  connect (mapStateToProps, {getProducts}) ( (prop
                 </div>
                 <div className="price-sorting">
                             <p className="price-sorting__header">sort by price:</p>
-                            <span  className="price-sorting__button" >Cheap first <i className="fas fa-sort-amount-down"></i></span> 
-                            <span  className="price-sorting__button" >Expensive first <i className="fas fa-sort-amount-up"></i></span>
+                            <span  className="price-sorting__button" onClick={sortCheap} >Cheap first <i className="fas fa-sort-amount-down"></i></span> 
+                            <span  className="price-sorting__button" onClick={sortExpensive} >Expensive first <i className="fas fa-sort-amount-up"></i></span>
                         </div>
                 </Row>
             </div>
             <div className="pagelist-content">
             <Row gutter={24}>
                 <Col xs={{span:24}} sm={{span:24}} md={{span:24}} lg={{span:18, push:6}}>
-                    
-                    {(products.length) ? products.map( item => 
-                        <Col xs={24} sm={24} md={12} lg={8} xl={8} key={item._id}>
-                            <ProductListItem product={item}></ProductListItem>
-                        </Col>
-                    ) : null}
+                    {(products.length) ? 
+                        products.map( item => 
+                            <Col xs={24} sm={24} md={12} lg={8} xl={8} key={item._id}>
+                                <ProductListItem product={item}></ProductListItem>
+                            </Col>
+                            )
+                         : null}
                     <Col xs={24} sm={24} md={24} lg={24} xl={24} className="pagelist-bottom">
                         {props.catalog.showMoreBtnStatus ?
                             <button className="button-outline" onClick={showMoreHandler}>Show More</button> : null
@@ -77,18 +125,12 @@ export const ProductListPage =  connect (mapStateToProps, {getProducts}) ( (prop
                     </Col>
                 </Col>
                 <Col xs={{span:24}} sm={{span:24}} md={{span:24}} lg={{span:6, pull:18}}>
-                    {/* <h4>number of displayed products</h4>
-                    <InputNumber min={3} max={30} defaultValue={9} onChange={displayProductsHandler} /> */}
-
                     <h4>Producer</h4>
-                    <Checkbox name="Jackson" >Jackson</Checkbox>
-                    <Checkbox name="Ibanez" >Ibanez</Checkbox>
-                    <Checkbox name="Hamer">Hamer</Checkbox>
-                    <Button >Sort by producer</Button>
-
+                    {producers.map(item => 
+                        <Checkbox name={item} onClick={checkboxHandler} key={item}>{item}</Checkbox>
+                    )}
                 </Col>
             </Row>
-
             </div>
         </div>
     )
