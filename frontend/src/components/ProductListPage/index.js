df976d20ab9cf9565256e693b142417b50e9f8cf
Maksim Popov <max.popov.mail@gmail.com>
2019-10-09 16:51:00 +0300

    fix conflicts

--- frontend/src/components/ProductListPage/index.js
+++ frontend/src/components/ProductListPage/index.js
@@ -36,12 +36,12 @@ export const ProductListPage =  connect (mapStateToProps, {getProducts}) ( (prop
         }
         props.getProducts(payload)
     };
-
+    console.log(props)
     props.catalog.categories.forEach(element => {
         if(element.name === props.match.params.category){
-            producers = element.producers              
-            }
-        })
+            producers = element.producers
+        }
+    })
 
     const checkboxHandler = (e) => {
         let checked = e.target.name;
@@ -83,7 +83,7 @@ export const ProductListPage =  connect (mapStateToProps, {getProducts}) ( (prop
         sortingExpensiveHandler = false;
         checkedArr= [];
         getProductsHandler();
-        
+
     }, [props.match.params.category]);
 
     useEffect(()=>{
@@ -94,44 +94,50 @@ export const ProductListPage =  connect (mapStateToProps, {getProducts}) ( (prop
         }
     }, [products])
 
-  
+
     return (
         <div className="pagelist-wrapper">
             <div className="page-top-info">
                 <Row type="flex" justify="space-between" align="middle" >
-                <div className="page-top-text">
+                    <div className="page-top-text">
                         <h4 className="category-header">Category: <span>{props.match.params.category}</span></h4>
-                </div>
-                <div className="price-sorting">
-                            <p className="price-sorting__header">sort by price:</p>
-                            <span  className="price-sorting__button" onClick={sortCheap} >Cheap first <i className="fas fa-sort-amount-down"></i></span> 
-                            <span  className="price-sorting__button" onClick={sortExpensive} >Expensive first <i className="fas fa-sort-amount-up"></i></span>
-                        </div>
+                    </div>
+                    <div className="price-sorting">
+                        <p className="price-sorting__header">sort by price:</p>
+                        {sortingCheapHandler ? <span  className="price-sorting__button black" onClick={sortCheap} >Cheap first <i className="fas fa-sort-amount-down"></i></span> : <span  className="price-sorting__button" onClick={sortCheap} >Cheap first <i className="fas fa-sort-amount-down"></i></span> }
+                        {sortingExpensiveHandler ? <span  className="price-sorting__button black" onClick={sortExpensive} >Expensive first <i className="fas fa-sort-amount-up"></i></span> : <span  className="price-sorting__button" onClick={sortExpensive} >Expensive first <i className="fas fa-sort-amount-up"></i></span> }
+                    </div>
                 </Row>
             </div>
             <div className="pagelist-content">
-            <Row gutter={24}>
-                <Col xs={{span:24}} sm={{span:24}} md={{span:24}} lg={{span:18, push:6}}>
-                    {(products.length) ? 
-                        products.map( item => 
-                            <Col xs={24} sm={24} md={12} lg={8} xl={8} key={item._id}>
-                                <ProductListItem product={item}></ProductListItem>
-                            </Col>
+                <Row gutter={24}>
+                    <Col xs={{span:24}} sm={{span:24}} md={{span:24}} lg={{span:21, push:3}}>
+                        {(products.length) ?
+                            products.map( item =>
+                                <Col xs={24} sm={12} md={8} lg={6} xl={6} key={item._id}>
+                                    <ProductListItem product={item}></ProductListItem>
+                                </Col>
                             )
-                         : null}
-                    <Col xs={24} sm={24} md={24} lg={24} xl={24} className="pagelist-bottom">
-                        {props.catalog.showMoreBtnStatus ?
-                            <button className="button-outline" onClick={showMoreHandler}>Show More</button> : null
-                        }
+                            : <h2 class="empty-text">There is no products</h2>}
+                        <Col xs={24} sm={24} md={24} lg={24} xl={24} className="pagelist-bottom">
+
+                            {
+                                products.length ?
+                                    props.catalog.showMoreBtnStatus ?
+                                        <button className="button-outline" onClick={showMoreHandler}>Show More</button> : null
+                                    : null
+                            }
+                        </Col>
                     </Col>
-                </Col>
-                <Col xs={{span:24}} sm={{span:24}} md={{span:24}} lg={{span:6, pull:18}}>
-                    <h4>Producer</h4>
-                    {producers.map(item => 
-                        <Checkbox name={item} onClick={checkboxHandler} key={item}>{item}</Checkbox>
-                    )}
-                </Col>
-            </Row>
+                    <Col xs={{span:24}} sm={{span:24}} md={{span:24}} lg={{span:3, pull:21}}>
+                        <div className='filters-wrapper'>
+                            <h4>Producer</h4>
+                            {producers.map(item =>
+                                <Checkbox name={item} onClick={checkboxHandler} key={item}>{item}</Checkbox>
+                            )}
+                        </div>
+                    </Col>
+                </Row>
             </div>
         </div>
     )
