2527eb8fd0cf4f90f4f41f23c5bdc4561e4257e3
Maksim Popov <max.popov.mail@gmail.com>
2019-10-10 13:08:58 +0300

    fixed merge by Andrey

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -14,7 +14,7 @@ export const Footer = () => {
             <Row gutter={16}>
                 <Col xs={24} sm={24} md={12} lg={12} xl={6} >
                     <div className="footer-logo text-center">
-                        <Link to='/'><span className='site-logo'>MUSIC-SHOP</span></Link>
+                        <Link to='/'><span className='site-logo'>MUZ-SHOP</span></Link>
                     </div>
                 </Col>
                 <Col xs={24} sm={24} md={12} lg={12} xl={6} >
